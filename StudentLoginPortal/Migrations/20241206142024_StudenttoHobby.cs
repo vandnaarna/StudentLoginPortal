dea39d1929a2ffbe6a1034c6d35280ffa204d268
DESKTOP-S4GDDSI\admin <vandana.chouhan@arnasoftech.com>
2024-12-06 20:29:59 +0530

    New commit

---
+++ StudentLoginPortal/Migrations/20241206142024_StudenttoHobby.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StudentLoginPortal.Migrations
+{
+    /// <inheritdoc />
+    public partial class StudenttoHobby : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+            migrationBuilder.AddColumn<int>(
+            name: "HId",
+            table: "StudentDetail",
+            nullable: false,
+            defaultValue: 0);
+
+            migrationBuilder.AddForeignKey(
+                          name: "FK_StudentDetail_StudentHobby_HId",
+                          table: "StudentDetail",
+                          column: "HId",
+                          principalTable: "StudentHobby",
+                          principalColumn: "Hid",
+                          onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StudentDetail_Hid",
+                table: "StudentDetail",
+                column: "Hid");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "StudentDetail");
+
+            migrationBuilder.DropTable(
+                name: "StudentHobby");
+        }
+    }
+}
--- StudentLoginPortal/Models/StudentDetail.cs
+++ StudentLoginPortal/Models/StudentDetail.cs
@@ -1,28 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace StudentLoginPortal.Models;
 
 public partial class StudentDetail
 {
     public int Id { get; set; }
-
+    [Required(ErrorMessage ="FistName is required")]
+    [StringLength(15)]
     public string FirstName { get; set; } = null!;
-
+    [Required(ErrorMessage = "LastName is required")]
+    [StringLength(15)]
     public string LastName { get; set; } = null!;
-
+    [Required(ErrorMessage = "Cityname is required")]
+    [StringLength(20)]
     public string City { get; set; } = null!;
-
+    [Required(ErrorMessage = "email is required")]
+    [EmailAddress]
     public string Email { get; set; } = null!;
-
+    [Required]
+    [MaxLength(10)]
     public int? Contact { get; set; }
-
+    [Required(ErrorMessage = "Classname is required")]
+    [StringLength(10)]
     public string? Class { get; set; }
-
+    [Required(ErrorMessage = "Password is required")]
+    [PasswordPropertyText]
     public string Password { get; set; } = null!;
-
-    public int Hid { get; set; }
     
-
-    public StudentHobby studentHobby { get; set; }
+    public int Hid { get; set; }
+    [ForeignKey("Hid")]
+    public  virtual StudentHobby StudentHobbies { get; set; }
 }
